9fc9b9e2c7192406d22654ec350f512925235849
vijay kodibailu <vijaykodibailu@yahoo.in>
2022-06-25 04:00:10 +0530

    initial commit Upload to heroko

---
+++ alembic/versions/cbd8b05bba26_create_posts_table.py
@@ -0,0 +1,24 @@
+"""create posts table
+
+Revision ID: cbd8b05bba26
+Revises: 
+Create Date: 2022-06-25 03:00:19.007265
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cbd8b05bba26'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
